8b1f3f6479def90782d87769e93e4e2499e72d4c
WK <850005050@nzse.ac.nz>
2025-03-19 18:55:26 +1300

    number test

--- GitHub Test/Program.cs
+++ GitHub Test/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Enter a 3: ");
+            int numerator = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter a 4: ");
+            int denominator = Convert.ToInt32(Console.ReadLine());
+
+            int result = numerator / denominator;
+            Console.WriteLine($"Result: {result}");
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Error: You cannot divide by zero.");
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Error: Please enter a valid number.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+        }
+        finally
+        {
+            Console.WriteLine("End of program.");
+        }
+    }
+}
